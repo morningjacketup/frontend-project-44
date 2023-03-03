75eb3440ef6f1ce118c10e9ca82c0da48f0b2888
Ivan Savvinov <morningjacketup@gmail.com>
2023-03-03 23:37:03 +0600

    finish last step

---
+++ src/games/brain-prime.js
@@ -0,0 +1,23 @@
+import startGame from '../index.js';
+import generateRandomNumber from '../utils.js';
+
+const isPrime = (number) => {
+    if (number < 2) {
+        return false;
+    }
+    for (let i = 2; i * i <= number; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const generateRoundData = () => {
+    const randomNumber = generateRandomNumber(0, 100);
+    return [`${randomNumber}`, isPrime(randomNumber) ? 'yes' : 'no'];
+}
+
+export default () => {
+    startGame('Answer "yes" if given number is prime. Otherwise answer "no".', generateRoundData);
+}
